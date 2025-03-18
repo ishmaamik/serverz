7f2885b57875601178b776aaf09a11d58701e9f3
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-19 05:18:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post("/api/execute", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client","dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
